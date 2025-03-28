a527ffc5d09f1cf04f379a6cd48cc430d0538c7c
Hanna Bodnarchuk <hannabodnarchuk.1@gmail.com>
2025-03-28 04:15:53 +0200

    test

--- server.js
+++ server.js
@@ -5,25 +5,91 @@ const wss = new WebSocket.Server({ port: PORT });
 const clients = new Set();
 const clientsData = new Map();
 
+//test
+
+function polydice(dice) {
+  return Math.floor(Math.random() * dice) + 1;
+}
+
+function sendToTeammates(messageJSON) {
+  let currentGame = messageJSON.gameId;
+  clients.forEach((client) => {
+    let clientData = clientsData.get(client);
+    if (clientData.gameId === currentGame) {
+      client.send(JSON.stringify(messageJSON));
+    }
+  });
+}
+
+function sendToTeammatesExceptMe(ws, messageJSON) {
+  let currentGame = messageJSON.gameId;
+  clients.forEach((client) => {
+    let clientData = clientsData.get(client);
+    if (clientData.gameId === currentGame && ws !== client) {
+      client.send(JSON.stringify(messageJSON));
+    }
+  });
+}
+
 wss.on("connection", (ws) => {
   ws.send("connected");
-  clients.add(ws);
-  wss.clients.forEach(function each(client) {
-    client.send('new client added. ' + wss.clients.size + ' active connections');
-  }); 
+
+  if (!clients.has(ws)) {
+    clients.add(ws);
+  }
+
+  ws.isAlive = true;
+
+  ws.on("pong", () => {
+    ws.isAlive = true;
+  });
 
   const interval = setInterval(() => {
-    ws.ping(); 
-  }, 30000);  
+    if (!ws.isAlive) {
+      ws.terminate(); // Закрывает соединение
+      return;
+    }
 
+    ws.isAlive = false;
+    ws.ping();
+  }, 30000);
+
+  // ----------- message -------------
   ws.on("message", (message) => {
-    clientsData.set(ws,JSON.parse(message));
-    ws.send(message + " returned");
+    const messageJSON = JSON.parse(message);
+    //service message after connection
+    switch (messageJSON.sectionName) {
+      case "connection":
+        clientsData.set(ws, messageJSON);
+        ws.send("user data is set: " + message);
+        break;
+      case "polydice":
+        let rollResults = [];
+        for (let i = 0; i < messageJSON.sectionInfo.rollNumbers; i++) {
+          rollResults.push(
+            polydice(messageJSON.sectionInfo.dice) +
+              messageJSON.sectionInfo.diceModifier
+          );
+        }
+        messageJSON.rollResults = rollResults;
+        sendToTeammates(messageJSON);
+        break;
+      case "chat":
+        let chatMessage = messageJSON.sectionInfo.chatMessage;
+        sendToTeammates(messageJSON);
+        break;
+      case "gameMap":
+        sendToTeammatesExceptMe(ws, messageJSON);
+        break;
+      case "globalMap":
+        break;
+      default:
+        ws.send("unknown message type: " + message);
+    }
   });
-  
 
   ws.on("close", () => {
-    //console.log("Client disconnected.");
     clearInterval(interval);
+    clients.delete(ws);
   });
 });
