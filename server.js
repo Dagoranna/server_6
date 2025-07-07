89d5beb66f6c6de7bd10e3981b490dc10380b700
Hanna Bodnarchuk <hannabodnarchuk.1@gmail.com>
2025-07-07 09:33:39 +0300

    more error handling added

--- server.js
+++ server.js
@@ -9,12 +9,18 @@ function polydice(dice) {
   return Math.floor(Math.random() * dice) + 1;
 }
 
+function wsSend(ws, data) {
+  if (ws.readyState === WebSocket.OPEN) {
+    ws.send(data);
+  }
+}
+
 function sendToTeammates(messageJSON) {
   let currentGame = messageJSON.gameId;
   clients.forEach((client) => {
     let clientData = clientsData.get(client);
-    if (clientData.gameId === currentGame) {
-      client.send(JSON.stringify(messageJSON));
+    if (clientData && clientData.gameId === currentGame) {
+      wsSend(client, JSON.stringify(messageJSON));
     }
   });
 }
@@ -23,8 +29,8 @@ function sendToTeammatesExceptMe(ws, messageJSON) {
   let currentGame = messageJSON.gameId;
   clients.forEach((client) => {
     let clientData = clientsData.get(client);
-    if (clientData.gameId === currentGame && ws !== client) {
-      client.send(JSON.stringify(messageJSON));
+    if (clientData && clientData.gameId === currentGame && ws !== client) {
+      wsSend(client, JSON.stringify(messageJSON));
     }
   });
 }
@@ -33,7 +39,7 @@ function findGames() {
   let DMs = {};
   clients.forEach((client) => {
     let clientData = clientsData.get(client);
-    if (clientData.user.userRole === "Master") {
+    if (clientData && clientData.user.userRole === "Master") {
       DMs[clientData.user.userEmail] = clientData.user.userName;
     }
   });
@@ -45,7 +51,7 @@ function findGames() {
 }
 
 wss.on("connection", (ws) => {
-  ws.send("connected");
+  wsSend(ws, "connected");
 
   if (!clients.has(ws)) {
     clients.add(ws);
@@ -69,67 +75,77 @@ wss.on("connection", (ws) => {
 
   // ----------- message -------------
   ws.on("message", (message) => {
-    const messageJSON = JSON.parse(message);
-    let currentData = clientsData.get(ws);
-    if (currentData) {
-      currentData.gameId = messageJSON.gameId;
-      clientsData.set(ws, currentData);
-    } else {
-      clientsData.set(ws, messageJSON);
-    }
-    let rollResults = [];
-    switch (messageJSON.sectionName) {
-      case "connection":
-        //service message after connection
-        /*
-        {
-          "gameId": someId,
-          "user":
-            {
-            "userRole":"Gamer/Master",
-            "userName":"SomeGamer",
-            "userColor":"Lime",
-            "userEmail": userEmail,
-            },
-          "sectionName":"connection"
-        }
-        */
-        ws.send("user data is set: " + message);
-        if (messageJSON.user.userRole === "Gamer" && !messageJSON.gameId) {
-          ws.send(findGames());
-        }
+    try {
+      let messageJSON;
+      try {
+        messageJSON = JSON.parse(message);
+      } catch (err) {
+        console.log("error in parsing message: " + message);
+        return;
+      }
 
-        break;
-      case "choosemaster":
-        ws.send(JSON.stringify(messageJSON));
-        break;
-      case "polydice":
-        if (messageJSON.sectionInfo.source === "polydice") {
-          for (let i = 0; i < messageJSON.sectionInfo.rollNumbers; i++) {
-            rollResults.push(
-              polydice(messageJSON.sectionInfo.dice) +
-                messageJSON.sectionInfo.diceModifier
-            );
+      let currentData = clientsData.get(ws);
+      if (currentData) {
+        currentData.gameId = messageJSON.gameId;
+        clientsData.set(ws, currentData);
+      } else {
+        clientsData.set(ws, messageJSON);
+      }
+      let rollResults = [];
+      switch (messageJSON.sectionName) {
+        case "connection":
+          //service message after connection
+          /*
+          {
+            "gameId": someId,
+            "user":
+              {
+              "userRole":"Gamer/Master",
+              "userName":"SomeGamer",
+              "userColor":"Lime",
+              "userEmail": userEmail,
+              },
+            "sectionName":"connection"
           }
-        } else if (messageJSON.sectionInfo.source === "charsheet") {
-          rollResults[0] =
-            polydice(20) + parseInt(messageJSON.sectionInfo.diceModifier);
-        }
-        messageJSON.rollResults = rollResults;
-        sendToTeammates(messageJSON);
-        break;
-      case "chat":
-        let chatMessage = messageJSON.sectionInfo.chatMessage;
-        sendToTeammates(messageJSON);
-        break;
-      case "gameMap":
-        sendToTeammatesExceptMe(ws, messageJSON);
-        break;
-      case "globalMap":
-        sendToTeammatesExceptMe(ws, messageJSON);
-        break;
-      default:
-        ws.send("unknown message type: " + message);
+          */
+          wsSend(ws, "user data is set: " + message);
+          if (messageJSON.user.userRole === "Gamer" && !messageJSON.gameId) {
+            wsSend(ws, findGames());
+          }
+
+          break;
+        case "choosemaster":
+          wsSend(ws, JSON.stringify(messageJSON));
+          break;
+        case "polydice":
+          if (messageJSON.sectionInfo.source === "polydice") {
+            for (let i = 0; i < messageJSON.sectionInfo.rollNumbers; i++) {
+              rollResults.push(
+                polydice(messageJSON.sectionInfo.dice) +
+                  messageJSON.sectionInfo.diceModifier
+              );
+            }
+          } else if (messageJSON.sectionInfo.source === "charsheet") {
+            rollResults[0] =
+              polydice(20) + parseInt(messageJSON.sectionInfo.diceModifier);
+          }
+          messageJSON.rollResults = rollResults;
+          sendToTeammates(messageJSON);
+          break;
+        case "chat":
+          sendToTeammates(messageJSON);
+          break;
+        case "gameMap":
+          sendToTeammatesExceptMe(ws, messageJSON);
+          break;
+        case "globalMap":
+          sendToTeammatesExceptMe(ws, messageJSON);
+          break;
+        default:
+          wsSend(ws, "unknown message type: " + message);
+      }
+    } catch (err) {
+      console.error("Error handling message:", err);
     }
   });
 
